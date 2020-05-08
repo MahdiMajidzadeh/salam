5ebc5903ceb6e0a804440d27294815a6b079bb59
Mahdi Majidzadeh <mahdi.majidzadeh@gmail.com>
2020-05-08 22:36:18 +0430

    fix style

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -1,46 +1,14 @@
-
 <!doctype html>
-<html lang="en">
+<html lang="fa">
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <link rel="icon" href="/docs/4.1/assets/img/favicons/favicon.ico">
-
-    <title>Fixed top navbar example for Bootstrap</title>
-
-    <!-- Bootstrap core CSS -->
-    <link href="../../dist/css/bootstrap.min.css" rel="stylesheet">
-    <link href="{{ mix('css/shards.css') }}" rel="stylesheet">
-
+    <title>@yield('title')</title>
+    <link href="{{ mix('css/nahar.css') }}" rel="stylesheet">
+    {{--<link rel="icon" href="/docs/4.1/assets/img/favicons/favicon.ico">--}}
 </head>
 
-<body>
-
-<nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
-    <a class="navbar-brand" href="#">Fixed navbar</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarCollapse">
-        <ul class="navbar-nav mr-auto">
-            <li class="nav-item active">
-                <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">Link</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link disabled" href="#">Disabled</a>
-            </li>
-        </ul>
-        <form class="form-inline mt-2 mt-md-0">
-            <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
-            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-        </form>
-    </div>
-</nav>
+<body class="rtl">
 
 <main role="main" class="container">
     <div class="jumbotron">
--- webpack.mix.js
+++ webpack.mix.js
@@ -1,15 +1,4 @@
 const mix = require('laravel-mix');
 
-/*
- |--------------------------------------------------------------------------
- | Mix Asset Management
- |--------------------------------------------------------------------------
- |
- | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel application. By default, we are compiling the Sass
- | file for the application as well as bundling up all the JS files.
- |js('resources/js/app.js', 'public/js')
- */
-
 mix.sass('resources/scss/nahar.scss', 'public/css')
     .version();
