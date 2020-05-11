cc2d19fa6c95f52221a6a6db1b1c20cde8218d11
Mahdi Majidzadeh <mahdi.majidzadeh@gmail.com>
2020-05-11 18:18:34 +0430

    login layout

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -4,29 +4,33 @@
 
 
 @section('content')
-    <div class="center-box">
-        <div class="card">
-            <div class="card-body">
-                @if (session('msg-error'))
-                    <div class="alert alert-danger alert-dismissible fade show" role="alert">
-                        {{ session('msg-error') }}
-                        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                            <span aria-hidden="true">&times;</span>
-                        </button>
+    <div class="row justify-content-center align-items-center">
+        <div class="col-12 col-md-4">
+            {{--<div class="center-box">--}}
+                <div class="card">
+                    <div class="card-body">
+                        @if (session('msg-error'))
+                            <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                                {{ session('msg-error') }}
+                                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                </button>
+                            </div>
+                        @endif
+                        <form action="{{ url('/') }}" method="post">
+                            @csrf
+                            <div class="form-group">
+                                <label>شماره همراه</label>
+                                <input type="text" name="mobile" class="form-control">
+                            </div>
+                            <div class="form-group">
+                                <label>رمز عبور</label>
+                                <input type="password" name="password" class="form-control">
+                            </div>
+                            <button type="submit" class="btn btn-primary">ورود</button>
+                        </form>
                     </div>
-                @endif
-                <form action="{{ url('/') }}" method="post">
-                    @csrf
-                    <div class="form-group">
-                        <label>شماره همراه</label>
-                        <input type="text" name="mobile" class="form-control">
-                    </div>
-                    <div class="form-group">
-                        <label>رمز عبور</label>
-                        <input type="password" name="password" class="form-control">
-                    </div>
-                    <button type="submit" class="btn btn-primary">ورود</button>
-                </form>
+                {{--</div>--}}
             </div>
         </div>
     </div>
