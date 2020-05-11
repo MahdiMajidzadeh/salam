6fa3376770836823c38f4d7401eb33f6d64c94e1
Mahdi Majidzadeh <mahdi.majidzadeh@gmail.com>
2020-05-11 15:16:42 +0430

    login check

--- app/Http/Controllers/AccountsController.php
+++ app/Http/Controllers/AccountsController.php
@@ -2,12 +2,28 @@
 
 namespace App\Http\Controllers;
 
+use App\Model\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 class AccountsController extends Controller
 {
     public function loginSubmit(Request $request)
     {
+        $mobile = $request->get('mobile');
+        $password = $request->get('password');
 
+        $user = User::where('mobile', $mobile)->first();
+
+        if($user) {
+            if ($user->password == Hash::make($password)) {
+                Auth::login($user, true);
+
+                return redirect('dashboard');
+            }
+        }
+
+        return redirect()->back()->with('msg-error', __('msg.login_wrong'));
     }
 }
--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -7,14 +7,23 @@
     <div class="center-box">
         <div class="card">
             <div class="card-body">
-                <form>
+                @if (session('msg-error'))
+                    <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                        {{ session('msg-error') }}
+                        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                @endif
+                <form action="{{ url('/') }}" method="post">
+                    @csrf
                     <div class="form-group">
                         <label>شماره همراه</label>
-                        <input type="text" class="form-control">
+                        <input type="text" name="mobile" class="form-control">
                     </div>
                     <div class="form-group">
                         <label>رمز عبور</label>
-                        <input type="password" class="form-control" >
+                        <input type="password" name="password" class="form-control">
                     </div>
                     <button type="submit" class="btn btn-primary">ورود</button>
                 </form>
