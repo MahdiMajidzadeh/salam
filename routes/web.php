0fa60b54f336d60499f9fa5b0e2d07ae964f3329
Zamani <alizayahack@gmail.com>
2020-05-25 13:05:35 +0430

    feat: Add restaurants and foods routes

--- routes/web.php
+++ routes/web.php
@@ -24,8 +24,10 @@ Route::group(['middleware' => 'auth', 'prefix' => 'admin'], function() {
     Route::post('/user/bulk', 'AdminUserController@bulkSubmit');
     Route::get('/restaurant/add', 'AdminFoodController@addRestaurant');
     Route::post('/restaurant/add', 'AdminFoodController@addRestaurantSubmit');
+    Route::get('/restaurants', 'AdminFoodController@restaurantsList');
     Route::get('/food/add', 'AdminFoodController@addFood');
     Route::post('/food/add', 'AdminFoodController@addFoodSubmit');
+    Route::get('/foods', 'AdminFoodController@foodsList');
     Route::get('/booking/add', 'AdminBookingController@add');
     Route::post('/booking/add', 'AdminBookingController@addSubmit');
     Route::get('/booking/day-list', 'AdminBookingController@dayList');
