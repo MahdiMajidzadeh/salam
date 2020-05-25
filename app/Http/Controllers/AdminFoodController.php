8935b33ec89bae009f4dace09c74c19670896510
Zamani <alizayahack@gmail.com>
2020-05-25 15:18:14 +0430

    feat: Add create booking validation

--- app/Http/Controllers/AdminFoodController.php
+++ app/Http/Controllers/AdminFoodController.php
@@ -6,7 +6,6 @@ use App\Enum\Role;
 use App\Model\Food;
 use App\Model\Restaurant;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Validator;
 
 class AdminFoodController extends Controller
 {
@@ -21,13 +20,9 @@ class AdminFoodController extends Controller
     {
         allowed(Role::FOOD_MANAGER);
 
-        $validator = Validator::make($request->all(), [
+        $request->validate([
             'name' => 'required|string|unique:restaurants,name'
         ]);
-
-        if ($validator->fails()) {
-            return redirect()->back()->withErrors($validator);
-        }
 
         $restaurant       = new Restaurant();
         $restaurant->name = $request->get('name');
@@ -56,15 +51,11 @@ class AdminFoodController extends Controller
     {
         allowed(Role::FOOD_MANAGER);
 
-        $validator = Validator::make($request->all(), [
+        $request->validate([
             'name' => 'required|string',
             'restaurant' => 'required|exists:restaurants,id',
             'price' => 'required|numeric'
         ]);
-
-        if ($validator->fails()) {
-            return redirect()->back()->withErrors($validator);
-        }
 
         $food                = new Food();
         $food->name          = $request->get('name');
