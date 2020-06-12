d4b7abf6b8df1e42c26606215a579c452daae6e2
Zamani <alizayahack@gmail.com>
2020-06-13 01:49:42 +0430

    feat: Add restaurants-bill.blade and users-bill.blade

---
+++ resources/views/admin_bill/restaurants-bill.blade.php
@@ -0,0 +1,51 @@
+@extends('master-dashboard')
+
+@section('title', 'حساب ماهیانه رستوران ها')
+
+@section('inner-content')
+    <div class="card my-4">
+        <div class="card-body p-3">
+            <form class="form-inline">
+                <div class="form-group ml-3">
+                    <label>ماه: </label>
+                    <select class="custom-select form-control-sm" name="month">
+                        @foreach(jMonths() as $monthNumber=>$monthName)
+                            <option value="{{ $monthNumber }}" {{$monthNumber == $month ? 'selected' : '' }}>
+                                {{ $monthName }}
+                            </option>
+                        @endforeach
+                    </select>
+                </div>
+                <div class="form-group ml-3">
+                    <label>سال: </label>
+                    <select class="custom-select form-control-sm" name="year">
+                        @foreach(range(1399,1450) as $yearNumber)
+                            <option value="{{$yearNumber}}" {{$yearNumber==$year ?'selected':''}}>
+                                {{ $yearNumber }}
+                            </option>
+                        @endforeach
+                    </select>
+                </div>
+                <button type="submit" class="btn btn-primary ml-3 btn-sm">فیلتر کن</button>
+            </form>
+        </div>
+    </div>
+    <table class="table table-striped">
+        <thead>
+        <tr>
+            <th scope="col">رستوران</th>
+            <th scope="col">جمع حساب</th>
+        </tr>
+        </thead>
+        <tbody>
+        @foreach($restaurantsBill as $restaurantBill)
+            <tr>
+                <td>{{$restaurantBill->first()->food->restaurant->name}}</td>
+                <td dir="ltr">
+                    {{ $restaurantBill->sum(function ($reservation) {return $reservation->price;})}}
+                </td>
+            </tr>
+        @endforeach
+        </tbody>
+    </table>
+@endsection
---
+++ resources/views/admin_bill/users-bill.blade.php
@@ -0,0 +1,53 @@
+@extends('master-dashboard')
+
+@section('title', 'حساب ماهیانه کاربران')
+
+@section('inner-content')
+    <div class="card my-4">
+        <div class="card-body p-3">
+            <form class="form-inline">
+                <div class="form-group ml-3">
+                    <label>ماه: </label>
+                    <select class="custom-select form-control-sm" name="month">
+                        @foreach(jMonths() as $monthNumber=>$monthName)
+                            <option value="{{ $monthNumber }}" {{$monthNumber == $month ? 'selected' : '' }}>
+                                {{ $monthName }}
+                            </option>
+                        @endforeach
+                    </select>
+                </div>
+                <div class="form-group ml-3">
+                    <label>سال: </label>
+                    <select class="custom-select form-control-sm" name="year">
+                        @foreach(range(1399,1450) as $yearNumber)
+                            <option value="{{$yearNumber}}" {{$yearNumber==$year ?'selected':''}}>
+                                {{ $yearNumber }}
+                            </option>
+                        @endforeach
+                    </select>
+                </div>
+                <button type="submit" class="btn btn-primary ml-3 btn-sm">فیلتر کن</button>
+            </form>
+        </div>
+    </div>
+    <table class="table table-striped">
+        <thead>
+        <tr>
+            <th scope="col">#</th>
+            <th scope="col">نام</th>
+            <th scope="col">جمع حساب</th>
+        </tr>
+        </thead>
+        <tbody>
+        @foreach($usersBill as $userBill)
+            <tr>
+                <td>{{$userBill->first()->user->id}}</td>
+                <td>{{ $userBill->first()->user->name }}</td>
+                <td dir="ltr">
+                    {{ $userBill->sum(function ($reservation) {return $reservation->price - $reservation->price_default;})}}
+                </td>
+            </tr>
+        @endforeach
+        </tbody>
+    </table>
+@endsection
