7f73dbf872ee41af6f438e4efb6e7368423715ba
Ali Zamani <alizayahack@gmail.com>
2020-06-20 18:11:27 +0430

    fix: Change FOOD_MANAGER to ACCOUNTANT_MANAGER in AdminBillController

--- app/Http/Controllers/AdminBillController.php
+++ app/Http/Controllers/AdminBillController.php
@@ -10,7 +10,7 @@ class AdminBillController extends Controller
 {
     public function usersBill()
     {
-        allowed(Role::FOOD_MANAGER);
+        allowed(Role::ACCOUNTANT_MANAGER);
 
         $data = getMonthDays();
 
@@ -27,7 +27,7 @@ class AdminBillController extends Controller
 
     public function restaurantsBill()
     {
-        allowed(Role::FOOD_MANAGER);
+        allowed(Role::ACCOUNTANT_MANAGER);
 
         $data = getMonthDays();
 
