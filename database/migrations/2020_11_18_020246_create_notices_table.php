d401bc0f8b1fa76c2e0cef92f34797c8da62173c
Mahdi Majidzadeh <mahdi.majidzadeh@gmail.com>
2020-11-18 02:06:11 +0330

    notice migration

---
+++ database/migrations/2020_11_18_020246_create_notices_table.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateNoticesTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('notices', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('banner')->nullable();
+            $table->text('content');
+            $table->timestamps();
+            $table->timestamp('started_at')->useCurrent();
+            $table->timestamp('ended_at')->useCurrent();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('notices');
+    }
+}
