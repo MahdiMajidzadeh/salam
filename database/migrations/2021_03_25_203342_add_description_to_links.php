91319057697522bf9a487ab33974d75d24a16929
Mahdi Majidzadeh <MahdiMajidzadeh@users.noreply.github.com>
2021-03-25 16:38:34 +0000

    Apply fixes from StyleCI

--- database/migrations/2021_03_25_203342_add_description_to_links.php
+++ database/migrations/2021_03_25_203342_add_description_to_links.php
@@ -1,12 +1,11 @@
 <?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
 
 class AddDescriptionToLinks extends Migration
 {
-
     public function up()
     {
         Schema::table('links', function (Blueprint $table) {
