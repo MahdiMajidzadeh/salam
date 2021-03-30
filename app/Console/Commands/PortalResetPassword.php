ea235f31a01958e677d6b530d839f01eed1575ee
Mahdi Majidzadeh <mahdi.majidzadeh@gmail.com>
2021-03-30 04:46:23 +0430

    cleanup commands

--- app/Console/Commands/PortalResetPassword.php
+++ app/Console/Commands/PortalResetPassword.php
@@ -6,27 +6,12 @@ use App\Model\User;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Hash;
 
-class ResetPassword extends Command
+class PortalResetPassword extends Command
 {
-    /**
-     * The name and signature of the console command.
-     *
-     * @var string
-     */
-    protected $signature = 'password:reset {mobile}';
+    protected $signature = 'portal:password-reset {mobile}';
 
-    /**
-     * The console command description.
-     *
-     * @var string
-     */
     protected $description = 'reset password for user';
 
-    /**
-     * Create a new command instance.
-     *
-     * @return void
-     */
     public function __construct()
     {
         parent::__construct();
--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -7,22 +7,17 @@ use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
 class Kernel extends ConsoleKernel
 {
-    protected $commands = [
-        //
-    ];
+    protected $commands = [];
 
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')->hourly();
-
         $schedule->command('backup:run --only-db')->hourly();
         $schedule->command('backup:clean')->weekly();
-//        $schedule->command('zoom:message')->dailyAt('12:30');
     }
 
     protected function commands()
     {
-        $this->load(__DIR__.'/Commands');
+        $this->load(__DIR__ . '/Commands');
 
         require base_path('routes/console.php');
     }
