9bde380659eb3fe6bbdd37897024de5db507a1ab
Mahdi Majidzadeh <MahdiMajidzadeh@users.noreply.github.com>
2021-04-02 16:15:18 +0000

    Apply fixes from StyleCI

--- database/migrations/2021_04_02_202843_create_author_book_table.php
+++ database/migrations/2021_04_02_202843_create_author_book_table.php
@@ -1,8 +1,8 @@
 <?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
 
 class CreateAuthorBookTable extends Migration
 {
@@ -15,7 +15,7 @@ class CreateAuthorBookTable extends Migration
             $table->timestamps();
         });
     }
-    
+
     public function down()
     {
         Schema::dropIfExists('author_book');
