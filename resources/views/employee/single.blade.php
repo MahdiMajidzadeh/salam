2e2ba2fa8779b60fb6a816c7268aff64c78e2ef0
Mahdi Majidzadeh <mahdi.majidzadeh@gmail.com>
2021-05-04 05:49:32 +0430

    change single page design

--- resources/views/employee/single.blade.php
+++ resources/views/employee/single.blade.php
@@ -3,52 +3,62 @@
 @section('title', $user->name)
 
 @section('inner-content')
-    <div class="row">
-        <div class="col-md-2 col-12">
+    <div class="row text-center">
+        <div class="col-12">
             @if(!is_null($user->avatar))
                 <img src="{{ asset(Illuminate\Support\Facades\Storage::url($user->avatar)) }}"
-                     class="img-thumbnail">
+                     class="rounded-circle" style="width: 160px">
             @else
-                <img src="{{ asset('img/default-user-image.png') }}" class="card-img">
+                <img src="{{ asset('img/default-user-image.png') }}" class="rounded-circle" style="width: 160px">
             @endif
         </div>
-        <div class="col-md-8 col-12">
-            <h1 class="h3 mt-3">{{ $user->name }}</h1>
+        <div class="col-12 mt-3">
+            <h1 class="h3">{{ $user->name }}</h1>
+        </div>
+        <div class="col-12 col-md-4 mx-md-auto mt-3">
+            {!! nl2br($user->biography)  !!}
+        </div>
+        <div class="col-12 text-black mt-3">
+            عضو تیم
             {{ optional($user->team)->name }}
-            -
+            از چپتر
             {{ optional($user->chapter)->name }}
-            -
-            ورود:
-            {{ jdfw_name($user->started_at) }}
-            <div class="my-3">
-                {!! nl2br($user->biography)  !!}
-            </div>
-            @isset($user->email)
-            <div class="info-box">
-                {{ $user->email }}
-            </div>
-            @endisset
-            @isset($user->email_basalam)
-            <div class="info-box">
-                {{ $user->email_basalam }}
-            </div>
-            @endisset
-            <div class="info-box">
+        </div>
+        <div class="col-12 mt-3 ltr">
+            <span class="info-box">
+                <i class="mdi mdi-cellphone"></i>
                 {{ $user->mobile }}
-            </div>
-            @isset($user->linkedin_url)
-            <div class="info-box">
-                {{ $user->linkedin_url }}
-            </div>
+            </span>
+        </div>
+        <div class="col-12 mt-3 ltr">
+            @isset($user->email_basalam)
+                <span class="info-box">
+                    <i class="mdi mdi-email"></i>
+                    {{ $user->email_basalam }}
+                </span>
             @endisset
-            @isset($user->virgol_url)
-                <div class="info-box">
-                    {{ $user->virgol_url }}
-                </div>
+            @isset($user->email)
+                <span class="info-box">
+                    <i class="mdi mdi-gmail"></i>
+                    {{ $user->email }}
+                </span>
             @endisset
         </div>
-        <div class="col-md-2 col-12">
-
+        <div class="col-12 mt-5 ltr">
+            @isset($user->linkedin_url)
+                <a href="{{ $user->linkedin_url }}" class="icon-social">
+                    <i class="mdi mdi-linkedin"></i>
+                </a>
+            @endisset
+            @isset($user->virgol_url)
+                <a href="{{ $user->virgol_url }}" class="icon-social">
+                    <i class="mdi mdi-comma"></i>
+                </a>
+            @endisset
+        </div>
+        <div class="col-12 mt-5">
+            ورود:
+            {{ jdfw_name($user->started_at) }}
         </div>
     </div>
 @endsection
